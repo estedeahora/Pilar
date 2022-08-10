5f35b3cae6eedc00017f0e8cba28170120c314b5
estedeahora <pabloserrati@gmail.com>
2022-08-10 15:42:26 -0300

    Limpiar script
    Sacar cosas de borrador.

--- analysis/scripts/02_data-wrangling.R
+++ analysis/scripts/02_data-wrangling.R
@@ -55,29 +55,3 @@ for(i in 1:length(l)){
 }
 
 rm(l, i, v, patron, ESC)
-
-# Transformar polígonos y lineas a puntos  -----------------------------------
-
-#
-# names(CARTO)
-#
-# CARTO$CALLE$highway |>
-#   table()
-#
-# CARTO$CALLE |> #st_drop_geometry() |> count(highway, sort = T)
-#   ggplot() +
-#   geom_sf(aes(color = highway))
-#
-# CARTO$CALLE |> st_drop_geometry() |> count(highway, sort = T)
-#
-# CARTO$CALLE |>
-#   # filter(highway == c( "residential", "service", "tertiary",
-#   #                      "motorway", "motorway_link") ) |>
-#   ggplot() +
-#   geom_sf(aes(color = highway), alpha = 0.5) +
-#   # geom_sf(data = CARTO$CALLE |> filter(highway == "living_street"),
-#   #         color = "red", size = 1) +
-#   geom_sf(data =CARTO$CALLE |> filter(str_detect( name, "Ramal Pilar")), color = "red", size = 2) +
-#   # geom_sf(data =CARTO$CALLE |> filter(highway  ==  "motorway"), color = "green") +
-#   geom_sf(data =CARTO$CALLE |> filter(str_detect( name, "Ruta")), color = "blue") +
-#   theme_void()
